4fa646dd3d308bca1b1d727dd342e2576e17d36b
YashSingla851 <90170318+YashSingla851@users.noreply.github.com>
2022-01-18 10:25:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,108 @@
+const timer = document.querySelector('#time');
+const start_btn = document.querySelector('#start_btn');
+const pause_btn = document.querySelector('#pause_btn');
+const reset_btn = document.querySelector('#reset_btn');
+const display = document.getElementById('clock');
+const audio = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-alarm-digital-clock-beep-989.mp3');
+audio.loop = true;
+let alarmTime = null;
+let alarmTimeout = null;
+let time = 0,
+  interval;
+
+function showTime() {
+  time += 1;
+  timer.innerHTML = toHHMMSS(time);
+}
+
+function start() {
+  interval = setInterval(showTime, 1000);
+  hideBtn([start_btn]);
+  showBtn([pause_btn, reset_btn]);
+}
+
+function pause() {
+  if (interval) {
+    clearInterval(interval);
+    interval = null;
+    pause_btn.innerHTML = 'RESUME';
+  } else {
+    interval = setInterval(showTime, 1000);
+    pause_btn.innerHTML = 'PAUSE';
+  }
+}
+
+function reset() {
+  clearInterval(interval);
+  interval = null;
+  pause_btn.innerHTML = 'PAUSE';
+  time = 0;
+  timer.innerHTML = toHHMMSS(time);
+  hideBtn([pause_btn, reset_btn]);
+  showBtn([start_btn]);
+}
+
+function toHHMMSS(time) {
+  let hours = Math.floor(time / 3600);
+  let minutes = Math.floor((time - hours * 3600) / 60);
+  let seconds = time - hours * 3600 - minutes * 60;
+
+  hours = `${hours}`.padStart(2, '0');
+  minutes = `${minutes}`.padStart(2, '0');
+  seconds = `${seconds}`.padStart(2, '0');
+
+  return hours + ':' + minutes + ':' + seconds;
+}
+
+function showBtn(btnArr) {
+  btnArr.forEach((btn) => (btn.style.display = 'inline-block'));
+}
+function hideBtn(btnArr) {
+  btnArr.forEach((btn) => (btn.style.display = 'none'));
+}
+
+function updateTime() {
+  const date = new Date();
+
+  const hour = formatTime(date.getHours());
+  const minutes = formatTime(date.getMinutes());
+  const seconds = formatTime(date.getSeconds());
+
+
+
+  display.innerText=`${hour} : ${minutes} : ${seconds}`
+}
+
+function formatTime(time) {
+  if ( time < 10 ) {
+      return '0' + time;
+  }
+  return time;
+}
+
+function setAlarmTime(value) {
+  alarmTime = value;
+}
+
+function setAlarm() {
+  if(alarmTime) {
+      const current = new Date();
+      const timeToAlarm = new Date(alarmTime);
+
+      if (timeToAlarm > current) {
+          const timeout = timeToAlarm.getTime() - current.getTime();
+          alarmTimeout = setTimeout(() => audio.play(), timeout);
+          alert('Alarm set');
+      }
+  }
+}
+
+function clearAlarm() {
+  audio.pause();
+  if (alarmTimeout) {
+      clearTimeout(alarmTimeout);
+      alert('Alarm cleared');
+  }
+}
+
+setInterval(updateTime, 1000);
